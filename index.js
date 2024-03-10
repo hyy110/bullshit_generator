e230f091e1234a59a2f228e1ec9d5f11380309a8
hyy110 <hqj678@gmail.com>
2024-03-10 16:37:13 +0800

    feat: add article generator

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { dirname, resolve, join } from 'path';
 
-import { generator } from './lib/generator';
-import { createRandomPicker } from './lib/random';
+import { generator } from './lib/generator.js';
+import { createRandomPicker } from './lib/random.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+function loadCorpus(src) {
+    const path = resolve(__dirname, src);
+    const data = readFileSync(path, {encoding: 'utf-8'});
+
+    return JSON.parse(data);
+}
+
+const corpus = loadCorpus('corpus/data.json');
+
+const pickTitle = createRandomPicker(corpus.title);
+const title = pickTitle();
+
+const article = generator(title, {corpus});
+console.log(`${title}\n\n       ${article.join('\n      ')}`);
