01f7912b07264e676ee15412b907cf1c9db4df9b
hyy110 <hqj678@gmail.com>
2024-03-10 16:10:51 +0800

    feat: add lib section

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-const url = import.meta.url;
-const path = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
-const data = readFileSync(path, {encoding: 'utf-8'})
+import { generator } from './lib/generator';
+import { createRandomPicker } from './lib/random';
 
-console.log(data);
-console.log(fileURLToPath(url));
-console.log(path);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
