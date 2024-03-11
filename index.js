d4cd1ad83bf334e8ad58a5f00a1cade2290ffb42
hyy110 <hqj678@gmail.com>
2024-03-11 13:23:22 +0800

    feat: filename add time

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname, resolve, join } from 'path';
+import { dirname, resolve } from 'path';
+import moment from 'moment';
 
 import { generator } from './lib/generator.js';
 import { createRandomPicker } from './lib/random.js';
@@ -16,14 +17,20 @@ function loadCorpus(src) {
 
 function writeArticle(title, article) {
     const text = `${title}\n\n   ${article.join('\n\n   ')}`;
-    const path = resolve(__dirname, 'output');
-    if (!existsSync(path)) {
-        mkdirSync(path);
+    const outputDir = resolve(__dirname, 'output');
+    const time = moment().format('YYYY-MM-DD');
+    const outputFile = resolve(outputDir, `${title}${time}.txt`);
+
+    if (!existsSync(outputDir)) {
+        mkdirSync(outputDir);
     }
-
-    const outputPath = join(path, 'output.txt');
-
-    writeFileSync(outputPath, text);
+    try {
+        writeFileSync(outputFile, text);
+    } catch(e) {
+        console.log(e);
+    }
+   
+    return outputFile;
 }
 
 const corpus = loadCorpus('corpus/data.json');
