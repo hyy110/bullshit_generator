febcb21a60286a1f7a3e4bc1e7358a8e05cbcc3e
hyy110 <hqj678@gmail.com>
2024-03-11 15:43:22 +0800

    feat: abstract function

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-import moment from 'moment';
-
+import { options } from './lib/cmd.js'
+import { loadCorpus, writeArticle } from './lib/corpus.js'
 import { generator } from './lib/generator.js';
 import { createRandomPicker } from './lib/random.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-function loadCorpus(src) {
-    const path = resolve(__dirname, src);
-    const data = readFileSync(path, {encoding: 'utf-8'});
-
-    return JSON.parse(data);
-}
-
-function writeArticle(title, article) {
-    const text = `${title}\n\n   ${article.join('\n\n   ')}`;
-    const outputDir = resolve(__dirname, 'output');
-    const time = moment().format('YYYY-MM-DD');
-    const outputFile = resolve(outputDir, `${title}${time}.txt`);
-
-    if (!existsSync(outputDir)) {
-        mkdirSync(outputDir);
-    }
-    try {
-        writeFileSync(outputFile, text);
-    } catch(e) {
-        console.log(e);
-    }
-   
-    return outputFile;
-}
-
 const corpus = loadCorpus('corpus/data.json');
+const title = options.title || createRandomPicker(corpus.title)();
+const article = generator(title, {corpus, ...options});
+const output = writeArticle(title, article);
 
-const pickTitle = createRandomPicker(corpus.title);
-const title = pickTitle();
-
-const article = generator(title, {corpus});
-
-writeArticle(title, article);
+console.log('生成成功！');
